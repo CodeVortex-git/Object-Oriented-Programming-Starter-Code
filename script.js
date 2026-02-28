c29772b60f11abe63f147477a9059e35ecfeb2f3
CodeVortex <codevortex001@gmail.com>
2026-02-28 21:00:42 +0100

    Learnt about prototype and __proto__ properties

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ const Person = function (name, birthYear) {
 
 const author = new Person('CodeVortex', 1999);
 console.log(author);
+
+const juliet = new Person('Juliet', 2000);
+const romeo = new Person('Romeo', 2000);
+
+// console.log(romeo, juliet);
+Person.prototype.calcCurAge = function () {
+  return `${this.name} is ${new Date().getFullYear() - this.birthYear} years old as of ${new Date().getFullYear()}`;
+};
+
+console.log(juliet.calcCurAge(), romeo.calcCurAge());
