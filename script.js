efc86eae75755b3f3bca70770357d481315dea8a
CodeVortex <codevortex001@gmail.com>
2026-03-02 14:47:30 +0100

    Completed Coding Challenge One

--- script.js
+++ script.js
@@ -18,4 +18,44 @@ Person.prototype.calcCurAge = function () {
   return `${this.name} is ${new Date().getFullYear() - this.birthYear} years old as of ${new Date().getFullYear()}`;
 };
 
-console.log(juliet.calcCurAge(), romeo.calcCurAge());
+// console.log(juliet.calcCurAge(), `${'&&'}`, romeo.calcCurAge());
+console.log(juliet.__proto__.__proto__.__proto__);
+
+const arr = [3, 4, 5, 5, 4, 5, 5, 2, 2, 2, 2, 2];
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+//Coding Challenge One
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('mercedes', 95);
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+bmw.brake();
+bmw.brake();
+bmw.brake();
+bmw.brake();
+
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
